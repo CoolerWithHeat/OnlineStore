7919130923c63b72856d09dbb5e22e76afc6f8cb
pcjoker04gmail.com <pcjoker04gmail.com>
2023-06-15 19:59:43 +0500

    Commit message here

--- djangofront/src/components.js
+++ djangofront/src/components.js
@@ -10,12 +10,6 @@ import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
-{/* <script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/jquery.scrolly.min.js"></script>
-<script src="assets/js/browser.min.js"></script>
-<script src="assets/js/breakpoints.min.js"></script>
-<script src="assets/js/util.js"></script>
-<script src="assets/js/main.js"></script> */}
 
 var host;
 export const Get_Static_Url = (filename)=>{
