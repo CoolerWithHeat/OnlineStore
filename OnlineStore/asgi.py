8b31bbfdbdf179a6af0d60073854cfbc6c83528a
pcjoker04gmail.com <pcjoker04gmail.com>
2023-06-27 23:39:30 +0500

    Daphne Errors Fix

--- OnlineStore/asgi.py
+++ OnlineStore/asgi.py
@@ -6,18 +6,16 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-
+from django.core.asgi import get_asgi_application
+default_http_settings = get_asgi_application()
 import os
 from ProductsBase import SocketHandlers
 from django.urls import re_path
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineStore.settings')
-
-default_http_settings = get_asgi_application()
 
 application = ProtocolTypeRouter({
 
